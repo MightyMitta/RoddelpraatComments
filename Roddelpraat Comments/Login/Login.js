927fe13fb2e0f98cf3ec68a84b0087f5dfbfc6d2
Mitchel <mitcheltieges@hotmail.com>
2023-03-13 18:35:25 +0100

    Fixed token save on login

--- Roddelpraat Comments/Login/Login.js
+++ Roddelpraat Comments/Login/Login.js
@@ -1,11 +1,31 @@
 document.querySelector('.login-submit').addEventListener("click", submitLogin);
 
-function submitLogin(){
-    chrome.storage.sync.set({
-        token: "SuperSecretToken"
-    }, function() {
-        setTimeout(function() {
-            window.location.replace(chrome.runtime.getURL('Popup.html'));
-        } , 750);
+async function submitLogin() {
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+
+    const data = {
+        username: username,
+        password: password
+    }
+
+    // request validation from server
+    await fetch('https://roddelpraat-api.azurewebsites.net/Login/', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            response.json().then(data => {
+                chrome.storage.local.set({ "token": data.token });
+                window.location.href = "Popup.html";
+            });
+        } else {
+            // to be replaced with a text on the page
+            alert("Username or password is incorrect");
+        }
     });
+
 }
